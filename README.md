23e29bcffec85675982d6fe8aaae8eaa27d9b24e
Jenna <jennamorabito25@gmail.com>
2023-06-05 13:04:24 +0200

    how to create a data streaming pipeline with kafka, spark, and presto

---
+++ README.md
@@ -0,0 +1,132 @@
+## Instrument API server to log events to Kafka
+```python
+#!/usr/bin/env python
+import json
+from kafka import KafkaProducer
+from flask import Flask, request
+
+app = Flask(__name__)
+producer = KafkaProducer(bootstrap_servers='kafka:29092')
+
+
+def log_to_kafka(topic, events):
+    events.update(request.headers)
+    producer.send(topic, json.dumps(events).encode())
+
+
+@app.route("/")
+def default_response():
+    default_event = {'event_type': 'default'}
+    log_to_kafka('events', default_event)
+    return "This is the default response!\n"
+
+@app.route("/purchase_a_sword")
+def purchase_a_sword():
+    purchase_sword_event = {'event_type': 'purchase_a_sword'}
+    log_to_kafka('events', purchase_sword_event)
+    return "Sword Purchased!\n"
+
+@app.route("/join_a_guild")
+def join_a_guild():
+    join_guild_event = {'event_type': 'join_a_guild'}
+    log_to_kafka('events', join_guild_event)
+    return "Guild Joined!\n"
+```
+
+## Use Spark streaming to filter select event types from Kafka, land them into HDFS/parquet to make them available for analysis
+Spin up cluster
+```bash
+docker-compose up -d
+```
+
+
+Create a topic
+```bash
+docker-compose exec kafka kafka-topics --create --topic events --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+```
+
+
+Run flask (host 0.0.0.0 allows us to recieve requests from anyone)
+```bash
+docker-compose exec mids env FLASK_APP=/w205/project-3-jennamorabito/game_api.py flask run --host 0.0.0.0
+```
+
+
+In a second terminal window, set up to watch Kafka
+```bash
+docker-compose exec mids kafkacat -C -b kafka:29092 -t events -o beginning
+```
+running kafkacat without -e so it will run continuously
+
+ 
+In a third terminal window, stream
+```bash
+docker-compose exec spark spark-submit /w205/project-3-jennamorabito/stream_and_hive.py
+```
+
+## Use Apache Bench to generate test data for the pipeline
+A script to generate events until inturrupted
+```bash
+#!/bin/bash
+
+while true; do 
+  docker-compose exec mids ab -n 10 -H "Host: user1.comcast.com" http://localhost:5000/ 
+  docker-compose exec mids ab -n 10 -H "Host: user1.comcast.com" http://localhost:5000/join_a_guild
+  docker-compose exec mids ab -n 10 -H "Host: user1.comcast.com" http://localhost:5000/purchase_a_sword
+  docker-compose exec mids ab -n 10 -H "Host: user2.att.com" http://localhost:5000/
+  docker-compose exec mids ab -n 10 -H "Host: user2.att.com" http://localhost:5000/join_a_guild
+  docker-compose exec mids ab -n 10 -H "Host: user2.att.com" http://localhost:5000/purchase_a_sword
+  sleep 10 
+done
+```
+In a fourth terminal window, call
+```bash
+sh data_gen.sh
+```
+
+
+In a fifth terminal window, check what it wrote to Hadoop
+```bash
+docker-compose exec cloudera hadoop fs -ls /tmp/sword_purchases
+```
+
+
+## Read it with Presto and query
+```bash
+docker-compose exec presto presto --server presto:8080 --catalog hive --schema default
+```
+
+How many times have users joined a guild or purchased a sword?
+```
+presto:default> select count(*) from join_guild;
+ _col0 
+-------
+  2540 
+(1 row)
+```
+```
+presto:default> select count(*) from sword_purchases;
+ _col0 
+-------
+  3740 
+(1 row)
+```
+
+How many distinct users are there?
+```
+presto:default> select count(distinct Host) from sword_purchases;
+ _col0 
+-------
+     2 
+(1 row)
+```
+
+How many swords did each user purchase?
+```
+presto:default> select Host, count(*) as count from sword_purchases group by Host;
+       Host        | count 
+-------------------+-------
+ user1.comcast.com |  1870 
+ user2.att.com     |  1870 
+(2 rows)
+```
---
+++ stream_and_hive.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, from_json
+from pyspark.sql.types import StructType, StructField, StringType
+
+# Tells Spark what schema to expect
+def event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+    ])
+
+# Creating different tables for purchasing swords and joining guilds by filtering the Kafka stream
+@udf('boolean')
+def is_sword_purchase(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'purchase_a_sword':
+        return True
+    return False
+
+@udf('boolean')
+def is_join_guild(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'join_a_guild':
+        return True
+    return False
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+    # pulling in the raw json format
+    raw_events = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .load()
+
+    # unrolling and filtering the Kakfa stream to land sword purchases in a normalized table
+    sword_purchases = raw_events \
+        .filter(is_sword_purchase(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+    
+    # unrolling and filtering the Kakfa stream to land guild joins in a normalized table
+    join_guild = raw_events \
+        .filter(is_join_guild(raw_events.value.cast('string'))) \
+        .select(raw_events.value.cast('string').alias('raw_event'),
+                raw_events.timestamp.cast('string'),
+                from_json(raw_events.value.cast('string'),
+                          event_schema()).alias('json')) \
+        .select('raw_event', 'timestamp', 'json.*')
+
+    # creating or over-writing the table
+    spark.sql("drop table if exists sword_purchases")
+    sword_sql_string = """
+        create external table if not exists sword_purchases (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/sword_purchases'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(sword_sql_string)
+    
+    spark.sql("drop table if exists join_guild")
+    guild_sql_string = """
+        create external table if not exists join_guild (
+            raw_event string,
+            timestamp string,
+            Accept string,
+            Host string,
+            `User-Agent` string,
+            event_type string
+            )
+            stored as parquet
+            location '/tmp/join_guild'
+            tblproperties ("parquet.compress"="SNAPPY")
+            """
+    spark.sql(guild_sql_string)
+
+    # landing the tables in parquet; updating every 60 seconds
+    sink1 = sword_purchases \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_sword_purchases") \
+        .option("path", "/tmp/sword_purchases") \
+        .trigger(processingTime="60 seconds") \
+        .start()
+
+    sink2 = join_guild \
+        .writeStream \
+        .format("parquet") \
+        .option("checkpointLocation", "/tmp/checkpoints_for_join_guild") \
+        .option("path", "/tmp/join_guild") \
+        .trigger(processingTime="60 seconds") \
+        .start()
+
+    # run until the user ctrl + C's 
+    sink1.awaitTermination()
+    sink2.awaitTermination()
+
+
+if __name__ == "__main__":
+    main()
